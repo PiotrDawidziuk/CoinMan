cb64c3ca2529e9e3b7f33c52969d65cb04bbe9fa
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2019-02-20 20:03:23 +0100

    animation

--- core/src/com/mygdx/game/CoinMan.java
+++ core/src/com/mygdx/game/CoinMan.java
@@ -10,19 +10,37 @@ public class CoinMan extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 	Texture[] man;
+	int manState=0;
+	int pause =0;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
 		man = new Texture[4];
-		man[0]
+		man[0] = new Texture("frame-1.png");
+		man[1] = new Texture("frame-2.png");
+		man[2] = new Texture("frame-3.png");
+		man[3] = new Texture("frame-4.png");
 	}
 
 	@Override
 	public void render () {
 		batch.begin();
 		batch.draw(background,0,0,Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
+
+		if (pause <5){
+			pause++;
+		}else {
+			pause = 0;
+
+			if (manState < 3) {
+				manState++;
+			} else {
+				manState = 0;
+			}
+		}
+		batch.draw(man[manState],Gdx.graphics.getWidth()/2-man[manState].getWidth()/2,Gdx.graphics.getHeight()/2-man[manState].getHeight()/2);
 		batch.end();
 	}
 	
